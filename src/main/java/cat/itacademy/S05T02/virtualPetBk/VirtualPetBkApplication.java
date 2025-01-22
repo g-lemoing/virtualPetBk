47cf9a23a329c85f0acd152364e38a26d06eee7e
g-lemoing <le.moing.gwenael@gmail.com>
2025-01-22 20:09:30 +0100

    Initial commit

---
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/VirtualPetBkApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.S05T02.virtualPetBk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualPetBkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualPetBkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/itacademy/S05T02/virtualPetBk/VirtualPetBkApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.S05T02.virtualPetBk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualPetBkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
