7fd59642809d60adbbf163bb7b2407ae61d2545a
g-lemoing <le.moing.gwenael@gmail.com>
2025-01-23 11:31:29 +0100

    UserServiceImpl > create

--- src/main/java/cat/itacademy/S05T02/virtualPetBk/service/UserServiceImpl.java
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/service/UserServiceImpl.java
@@ -1,10 +1,14 @@
 package cat.itacademy.S05T02.virtualPetBk.service;
 
 import cat.itacademy.S05T02.virtualPetBk.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
     private UserRepository userRepository;
     @Override
