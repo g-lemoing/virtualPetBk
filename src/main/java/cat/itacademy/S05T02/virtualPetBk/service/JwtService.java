3050ade0cfeee32f42ca23d18814419bb7d6ae0c
g-lemoing <le.moing.gwenael@gmail.com>
2025-01-23 17:31:57 +0100

    JwtService > initial interface and implementation

---
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/service/JwtService.java
@@ -0,0 +1,9 @@
+package cat.itacademy.S05T02.virtualPetBk.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface JwtService {
+    String extractUserName(String token);
+    String generateToken(UserDetails userDetails);
+    boolean isTokenValid(String token, UserDetails userDetails);
+}
