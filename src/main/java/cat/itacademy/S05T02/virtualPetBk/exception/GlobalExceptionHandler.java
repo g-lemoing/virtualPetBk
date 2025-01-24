e81ca97c6ab6c2eb587f2606fd0ee7ffd899108d
g-lemoing <le.moing.gwenael@gmail.com>
2025-01-24 14:06:00 +0100

    GlobalExceptionHandler > add ProblemDetail

--- src/main/java/cat/itacademy/S05T02/virtualPetBk/exception/GlobalExceptionHandler.java
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package cat.itacademy.S05T02.virtualPetBk.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,9 +10,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    ProblemDetail problemDetail = null;
+
     @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<String> handleBadCredentialsException(BadCredentialsException e){
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    public ProblemDetail handleBadCredentialsException(BadCredentialsException e){
+        problemDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), e.getMessage());
+        problemDetail.setProperty("description", "The username or password is incorrect");
+        return problemDetail;
     }
 
 }
