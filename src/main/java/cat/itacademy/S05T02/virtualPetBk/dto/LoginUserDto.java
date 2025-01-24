01d4f07fb40aef23344912dbadc2dd955d0cc9ea
g-lemoing <le.moing.gwenael@gmail.com>
2025-01-24 10:20:16 +0100

    Dtos > create LoginUserDto and RegisterUserDto

---
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/dto/LoginUserDto.java
@@ -0,0 +1,22 @@
+package cat.itacademy.S05T02.virtualPetBk.dto;
+
+public class LoginUserDto {
+    private String userName;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
