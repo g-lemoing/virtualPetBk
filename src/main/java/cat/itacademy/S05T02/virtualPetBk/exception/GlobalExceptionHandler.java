abd236e4ff5ee286f90d0d8dbb5648de4ca67c80
g-lemoing <le.moing.gwenael@gmail.com>
2025-01-24 13:35:46 +0100

    GlobalExceptionHandler > create

---
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package cat.itacademy.S05T02.virtualPetBk.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<String> handleBadCredentialsException(BadCredentialsException e){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    }
+
+}
