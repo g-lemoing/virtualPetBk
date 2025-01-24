62fd2a191091e701098dc820be3a7a70fd38062c
g-lemoing <le.moing.gwenael@gmail.com>
2025-01-24 13:36:01 +0100

    LoginResponseDto > create

---
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/dto/LoginResponseDto.java
@@ -0,0 +1,22 @@
+package cat.itacademy.S05T02.virtualPetBk.dto;
+
+public class LoginResponseDto {
+    private String token;
+    private long expiresIn;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public long getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(long expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+}
