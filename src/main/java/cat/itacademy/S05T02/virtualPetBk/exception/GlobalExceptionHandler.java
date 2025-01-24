2de33e355b9b5d5d5c5f40aa8f9145ae5db7d4d5
g-lemoing <le.moing.gwenael@gmail.com>
2025-01-24 14:13:28 +0100

    GlobalExceptionHandler > add handlers

--- src/main/java/cat/itacademy/S05T02/virtualPetBk/exception/GlobalExceptionHandler.java
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/exception/GlobalExceptionHandler.java
@@ -1,9 +1,9 @@
 package cat.itacademy.S05T02.virtualPetBk.exception;
 
-import org.springframework.http.HttpStatus;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,6 +16,20 @@ public class GlobalExceptionHandler {
     public ProblemDetail handleBadCredentialsException(BadCredentialsException e){
         problemDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(401), e.getMessage());
         problemDetail.setProperty("description", "The username or password is incorrect");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ProblemDetail handleBadCredentialsException(ExpiredJwtException e){
+        problemDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), e.getMessage());
+        problemDetail.setProperty("description", "The JWT token has expired");
+        return problemDetail;
+    }
+
+    @ExceptionHandler(SignatureException.class)
+    public ProblemDetail handleBadCredentialsException(SignatureException e){
+        problemDetail = ProblemDetail.forStatusAndDetail(HttpStatusCode.valueOf(403), e.getMessage());
+        problemDetail.setProperty("description", "The JWT signature is invalid");
         return problemDetail;
     }
 
