a1f62b8d06e8034afec1869aadd7fbea7e5d0377
g-lemoing <le.moing.gwenael@gmail.com>
2025-01-24 13:39:30 +0100

    VirtualPetBkApplication > Add OpenAPIDefinition

--- src/main/java/cat/itacademy/S05T02/virtualPetBk/VirtualPetBkApplication.java
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/VirtualPetBkApplication.java
@@ -1,9 +1,11 @@
 package cat.itacademy.S05T02.virtualPetBk;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class VirtualPetBkApplication {
 
 	public static void main(String[] args) {
