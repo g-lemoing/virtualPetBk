49d061a2d6fb21c8e181cfbab3e40c67b78f9668
g-lemoing <le.moing.gwenael@gmail.com>
2025-01-29 11:44:57 +0100

    UserController > add @PreAuthorize on findAllUsers()

--- src/main/java/cat/itacademy/S05T02/virtualPetBk/controller/UserController.java
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/controller/UserController.java
@@ -4,6 +4,7 @@ import cat.itacademy.S05T02.virtualPetBk.model.User;
 import cat.itacademy.S05T02.virtualPetBk.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class UserController {
     private UserServiceImpl userService;
 
     @GetMapping("/")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN')")
     public ResponseEntity<List<User>> findAllUsers(){
         List<User> users = userService.findAllUsers();
         return ResponseEntity.ok(users);
