084e7014eb0a7f2db44da6424fa7a947dedd75b4
g-lemoing <le.moing.gwenael@gmail.com>
2025-01-30 12:17:17 +0100

    UserPetCreateDto> create

---
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/dto/UserPetCreateDto.java
@@ -0,0 +1,55 @@
+package cat.itacademy.S05T02.virtualPetBk.dto;
+
+import cat.itacademy.S05T02.virtualPetBk.model.Animal;
+import cat.itacademy.S05T02.virtualPetBk.model.PetColor;
+
+import java.awt.*;
+
+public class UserPetCreateDto {
+    private int userId;
+    private Animal animal;
+    private String petName;
+    private PetColor petColor;
+
+    public UserPetCreateDto() {
+    }
+
+    public UserPetCreateDto(int userId, Animal animal, String petName, PetColor petColor) {
+        this.userId = userId;
+        this.animal = animal;
+        this.petName = petName;
+        this.petColor = petColor;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public Animal getAnimal() {
+        return animal;
+    }
+
+    public void setPetId(Animal animal) {
+        this.animal = animal;
+    }
+
+    public String getPetName() {
+        return petName;
+    }
+
+    public void setPetName(String petName) {
+        this.petName = petName;
+    }
+
+    public PetColor getPetColor() {
+        return petColor;
+    }
+
+    public void setPetColor(PetColor petColor) {
+        this.petColor = petColor;
+    }
+}
