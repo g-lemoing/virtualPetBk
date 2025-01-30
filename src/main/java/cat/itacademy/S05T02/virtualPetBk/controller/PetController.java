8680a1af8d2e277e60efed3ebf46de32b7b4f236
g-lemoing <le.moing.gwenael@gmail.com>
2025-01-30 12:11:13 +0100

    PetController> add endpoints /update, /delete and /read

--- src/main/java/cat/itacademy/S05T02/virtualPetBk/controller/PetController.java
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/controller/PetController.java
@@ -1,14 +1,16 @@
 package cat.itacademy.S05T02.virtualPetBk.controller;
 
 import cat.itacademy.S05T02.virtualPetBk.dto.UserPetCreateDto;
+import cat.itacademy.S05T02.virtualPetBk.model.User;
 import cat.itacademy.S05T02.virtualPetBk.model.UserPet;
 import cat.itacademy.S05T02.virtualPetBk.service.PetServiceImpl;
+import org.apache.logging.log4j.core.config.plugins.validation.constraints.Required;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pet")
@@ -21,5 +23,25 @@ public class PetController {
                                           AuthenticationController authenticationController){
         UserPet userPet = petService.createUserPet(userPetCreateDto);
         return ResponseEntity.ok(userPet);
+    }
+    @GetMapping("/read")
+    ResponseEntity<List<UserPet>> getAllPets(AuthenticationController authenticationController){
+        List<UserPet> userPets = petService.getAllPets();
+        return ResponseEntity.ok(userPets);
+    }
+
+    @PutMapping("/update/{petUserId}/{action}")
+    ResponseEntity<UserPet> updatePet(AuthenticationController authenticationController,
+                                      @PathVariable @Required int petUserId,
+                                      @PathVariable @Required String action){
+        UserPet userPet = petService.updateUserPet(petUserId, action);
+        return ResponseEntity.ok(userPet);
+    }
+
+    @DeleteMapping("/delete/{petUserId}")
+    ResponseEntity<Void> deletePet(AuthenticationController authenticationController,
+                                   @PathVariable @Required int petUserId){
+        petService.deleteUserPet(petUserId);
+        return ResponseEntity.noContent().build();
     }
 }
