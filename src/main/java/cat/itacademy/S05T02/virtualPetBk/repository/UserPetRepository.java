8f3905dcf209a0aded33b7e9e70043038ff54b5d
g-lemoing <le.moing.gwenael@gmail.com>
2025-02-02 13:53:17 +0100

    UserPetRepository.java> remove unused imports

--- src/main/java/cat/itacademy/S05T02/virtualPetBk/repository/UserPetRepository.java
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/repository/UserPetRepository.java
@@ -1,6 +1,5 @@
 package cat.itacademy.S05T02.virtualPetBk.repository;
 
-import cat.itacademy.S05T02.virtualPetBk.model.User;
 import cat.itacademy.S05T02.virtualPetBk.model.UserPet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
