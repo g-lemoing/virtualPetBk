1a6ca051cc1a00709933e868ff04aa3de09be795
g-lemoing <le.moing.gwenael@gmail.com>
2025-02-02 13:51:45 +0100

    Animal> modify description attribute

--- src/main/java/cat/itacademy/S05T02/virtualPetBk/model/Animal.java
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/model/Animal.java
@@ -1,9 +1,7 @@
 package cat.itacademy.S05T02.virtualPetBk.model;
 
 public enum Animal {DOG("Dog"), LION("Lion"), KANGAROO("Kangaroo"), KOALA("Koala");
-    private String animalDesc;
 
     Animal(String animalDesc) {
-        this.animalDesc = animalDesc;
     }
 }
