76c1c05aba5f7b47ee0819de7646f89ab5fb64b9
g-lemoing <le.moing.gwenael@gmail.com>
2025-02-12 19:48:22 +0100

    AuthenticationController> add CrossOrigin annotation to class

--- src/main/java/cat/itacademy/S05T02/virtualPetBk/controller/AuthenticationController.java
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/controller/AuthenticationController.java
@@ -11,9 +11,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:5173")
 public class AuthenticationController {
     private final JwtServiceImpl jwtService;
     private final AuthenticationService authenticationService;
