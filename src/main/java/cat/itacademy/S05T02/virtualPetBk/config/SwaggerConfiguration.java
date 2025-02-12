9ad56aa46ef6e73a1131a718d26ec37ee0b0aa6a
g-lemoing <le.moing.gwenael@gmail.com>
2025-02-12 19:45:56 +0100

    SwaggerConfiguration> leave empty

--- src/main/java/cat/itacademy/S05T02/virtualPetBk/config/SwaggerConfiguration.java
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/config/SwaggerConfiguration.java
@@ -1,17 +1,8 @@
 package cat.itacademy.S05T02.virtualPetBk.config;
 
-import org.springdoc.core.models.GroupedOpenApi;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfiguration {
-//
-//    @Bean
-//    GroupedOpenApi api() {
-//        return GroupedOpenApi.builder()
-//                .group("virtualPet")
-//                .pathsToMatch("/auth/**", "/pet/**")
-//                .build();
-//    }
+
 }
