a82655a404e1a5d1fe746665b77c9d1f67ce64bc
g-lemoing <le.moing.gwenael@gmail.com>
2025-02-12 19:45:15 +0100

    Animal> change DOG <-- MONKEY

--- src/main/java/cat/itacademy/S05T02/virtualPetBk/model/Animal.java
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/model/Animal.java
@@ -1,6 +1,6 @@
 package cat.itacademy.S05T02.virtualPetBk.model;
 
-public enum Animal {DOG("Dog"), LION("Lion"), KANGAROO("Kangaroo"), KOALA("Koala");
+public enum Animal {MONKEY("Monkey"), LION("Lion"), KANGAROO("Kangaroo"), KOALA("Koala");
 
     Animal(String animalDesc) {
     }
