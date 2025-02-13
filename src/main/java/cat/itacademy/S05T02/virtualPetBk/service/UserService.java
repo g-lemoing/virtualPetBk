24d097fd2e4047a5d78b52667f011c75003ae759
g-lemoing <le.moing.gwenael@gmail.com>
2025-02-13 12:25:50 +0100

    UserController.java & UserService(Impl)> remove classes

--- src/main/java/cat/itacademy/S05T02/virtualPetBk/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.S05T02.virtualPetBk.service;
-
-import cat.itacademy.S05T02.virtualPetBk.model.User;
-
-import java.util.List;
-
-public interface UserService {
-    List<User> findAllUsers();
-}
