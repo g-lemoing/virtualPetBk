c5d2e53754ee44467f215b8b69d9a13169210037
g-lemoing <le.moing.gwenael@gmail.com>
2025-02-19 20:15:06 +0100

    AuthenticationController > retrieve userRole in login, to add to LoginResponseDto

--- src/main/java/cat/itacademy/S05T02/virtualPetBk/controller/AuthenticationController.java
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/controller/AuthenticationController.java
@@ -3,10 +3,12 @@ package cat.itacademy.S05T02.virtualPetBk.controller;
 import cat.itacademy.S05T02.virtualPetBk.dto.LoginResponseDto;
 import cat.itacademy.S05T02.virtualPetBk.dto.LoginUserDto;
 import cat.itacademy.S05T02.virtualPetBk.dto.RegisterUserDto;
+import cat.itacademy.S05T02.virtualPetBk.model.Role;
 import cat.itacademy.S05T02.virtualPetBk.model.User;
 import cat.itacademy.S05T02.virtualPetBk.service.AuthenticationServiceImpl;
 import cat.itacademy.S05T02.virtualPetBk.service.JwtServiceImpl;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,12 +35,16 @@ public class AuthenticationController {
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDto> authenticate(@RequestBody LoginUserDto loginUserDto) {
         UserDetails authenticatedUser = authenticationService.login(loginUserDto);
+        String userRole = authenticatedUser.getAuthorities()
+                .stream().findFirst().map(GrantedAuthority::getAuthority).orElse("ROLE_USER");
 
         String jwtToken = jwtService.generateToken(authenticatedUser);
 
-        LoginResponseDto loginResponse = new LoginResponseDto();
-        loginResponse.setToken(jwtToken);
-        loginResponse.setExpiresIn(jwtService.getExpirationTime());
+        LoginResponseDto loginResponse = new LoginResponseDto(
+                jwtToken, jwtService.getExpirationTime(),
+                authenticatedUser.getUsername(), userRole);
+//        loginResponse.setToken(jwtToken);
+//        loginResponse.setExpiresIn(jwtService.getExpirationTime());
 
         return ResponseEntity.ok(loginResponse);
     }
