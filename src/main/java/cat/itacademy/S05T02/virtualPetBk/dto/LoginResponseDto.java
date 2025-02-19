dd6e9c64b7ccac6ea11e5f5ad063b7565a1af217
g-lemoing <le.moing.gwenael@gmail.com>
2025-02-19 20:14:03 +0100

    LoginResponseDto > add userName and userRole attributes

--- src/main/java/cat/itacademy/S05T02/virtualPetBk/dto/LoginResponseDto.java
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/dto/LoginResponseDto.java
@@ -1,10 +1,23 @@
 package cat.itacademy.S05T02.virtualPetBk.dto;
 
+import cat.itacademy.S05T02.virtualPetBk.model.Role;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class LoginResponseDto {
     private String token;
     private long expiresIn;
+    private String userName;
+    private String userRole;
+
+    public LoginResponseDto() {
+    }
+
+    public LoginResponseDto(String token, long expiresIn, String userName, String userRole) {
+        this.token = token;
+        this.expiresIn = expiresIn;
+        this.userName = userName;
+        this.userRole = userRole;
+    }
 
     @JsonProperty("token")
     public String getToken() {
@@ -22,5 +35,21 @@ public class LoginResponseDto {
 
     public void setExpiresIn(long expiresIn) {
         this.expiresIn = expiresIn;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserRole() {
+        return userRole;
     }
 }
