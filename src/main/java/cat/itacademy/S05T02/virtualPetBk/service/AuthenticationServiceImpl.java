9c890f4f43f3d67342bb96db7fa40c7570b268f2
g-lemoing <le.moing.gwenael@gmail.com>
2025-02-19 09:51:51 +0100

    AuthenticationServiceImpl > Optional<User> <-- List<User> in findByUserName

--- src/main/java/cat/itacademy/S05T02/virtualPetBk/service/AuthenticationServiceImpl.java
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/service/AuthenticationServiceImpl.java
@@ -3,6 +3,7 @@ package cat.itacademy.S05T02.virtualPetBk.service;
 import cat.itacademy.S05T02.virtualPetBk.dto.LoginUserDto;
 import cat.itacademy.S05T02.virtualPetBk.dto.RegisterUserDto;
 import cat.itacademy.S05T02.virtualPetBk.exception.UserNameAlreadyExistsException;
+import cat.itacademy.S05T02.virtualPetBk.exception.UserNotFoundException;
 import cat.itacademy.S05T02.virtualPetBk.model.Role;
 import cat.itacademy.S05T02.virtualPetBk.model.User;
 import cat.itacademy.S05T02.virtualPetBk.repository.UserRepository;
@@ -11,7 +12,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService{
@@ -30,7 +31,7 @@ public class AuthenticationServiceImpl implements AuthenticationService{
     @Override
     public User signup(RegisterUserDto registerUser){
         String submittedName = registerUser.getUserName();
-        if(findByUsername(submittedName).isPresent()) throw new UserNameAlreadyExistsException(submittedName);
+        if(!findByUsername(submittedName).isEmpty()) throw new UserNameAlreadyExistsException(submittedName);
 
         User user = new User();
         user.setUserName(submittedName);
@@ -47,11 +48,14 @@ public class AuthenticationServiceImpl implements AuthenticationService{
                         loginUser.getPassword())
         );
         return userRepository.findByUserName(loginUser.getUserName())
-                .orElseThrow();
+                .stream()
+                .findFirst()
+                .orElseThrow(() -> new UserNotFoundException(loginUser.getUserName()));
     }
 
     @Override
-    public Optional<User> findByUsername(String userName) {
-        return userRepository.findByUserName(userName);
+    public List<User> findByUsername(String userName) {
+        List<User> users = userRepository.findByUserName(userName);
+        return users;
     }
 }
