b26a5fe11ea2e2deb542335ddce258af47ce6851
g-lemoing <le.moing.gwenael@gmail.com>
2025-02-19 10:00:50 +0100

    UserRepository > Optional<User> <-- List<User>

--- src/main/java/cat/itacademy/S05T02/virtualPetBk/repository/UserRepository.java
+++ src/main/java/cat/itacademy/S05T02/virtualPetBk/repository/UserRepository.java
@@ -3,10 +3,9 @@ package cat.itacademy.S05T02.virtualPetBk.repository;
 import cat.itacademy.S05T02.virtualPetBk.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findByUserName(String userName);
+    List<User> findByUserName(String userName);
 }
